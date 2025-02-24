90b14e800c73b4c813bc99f6bef8e7ec7f132165
AmenhotepPrince <amenhotepn@nycstudents.net>
2025-02-23 21:09:24 -0500

    Update Emoji_Project.py

--- Emoji_Project/Emoji_Project.py
+++ Emoji_Project/Emoji_Project.py
@@ -1,21 +1,23 @@
 from cmu_graphics import *
+Rect(0,0,400,400,fill='blue')
+
 #The face
-Circle(200,75,50, fill=gradient('white', 'yellow'), border='black', borderWidth=1)
+Star(200,75,100,5,fill='lightBlue',border='black',borderWidth=2)
+Circle(200,75,50, fill=gradient('yellow', 'orange'), border='black', borderWidth=2)
 #eyes & nose
-Oval(175,65,17,35)
-Oval(225,65,17,35)
+Oval(175,65,13,32)
+Oval(225,65,13,32)
 Line(200,62,207,75)
 Line(207,75,200,77)
-#smile:The lips
+#smile
 Line(175,110,225,110)
 Line(165,95,235,95)
 Line(175,110,165,95)
 Line(225,110,235,95)
-#smile:The teeth
-Polygon(175,110,225,110,235,95,165,95,fill='white')
+Polygon(175,110,225,110,235,95,165,95,fill=gradient('white','grey'))
 Line(175,110,175,95)
 Line(191.6,110,191.6,95)
 Line(208.2,110,208.2,95)
 Line(225,110,225,95)
-#smile:The 
+
 cmu_graphics.run()
